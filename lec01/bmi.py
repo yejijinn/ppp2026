a2067a4a3388e30c600ce5c806c86fce18ffb60c
yejijinn <yejinbae0628@gmail.com>
2026-03-19 11:23:19 +0900

    Add files via upload

---
+++ lec01/bmi.py
@@ -0,0 +1,5 @@
+weight = 60
+height = 1.7
+BMI = (weight)/(height/100)**2
+print(f"키가 {height}m이고, 몸무게가 {weight}kg이라면, BMI는 {BMI} 입니다.")
+
---
+++ lec01/square.py
@@ -0,0 +1,5 @@
+w = 5
+h = 4
+u = 3
+square = (u+w)*h/2
+print(f"윗변{u},아랫변{w},높이{h} => 넓이 {square}")
