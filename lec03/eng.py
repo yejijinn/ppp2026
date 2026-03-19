2510696de94ecd76bfe58a67d4897ac27c05bca2
yejijinn <yejinbae0628@gmail.com>
2026-03-19 11:23:56 +0900

    Add files via upload

---
+++ lec03/eng.py
@@ -0,0 +1,13 @@
+#eng =(float(input("영단어를 입력해주세요.")))
+text = "hello world"
+
+print (len(text))
+# 텍스트 길이
+
+#print (text.lower()) 소문자
+print (text.upper())
+
+print("="*30)
+
+print (text[:3])
+print (text[9:]) #[-2:] 해도됨
