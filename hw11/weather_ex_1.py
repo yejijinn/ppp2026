a0760f523de358ee6118bb43a138c2da4bc51ff2
yejijinn <yejinbae0628@gmail.com>
2026-04-28 23:07:29 +0900

    Add files via upload

---
+++ hw11/weather_ex_1.py
@@ -0,0 +1,38 @@
+def read_tavgs(weather_filename):
+    dataset = []
+    with open (weather_filename) as f:
+        lines = f.readlines()
+        for line in lines[1:]:
+            tokens = line.split(",")
+            dataset.append(float(tokens[4]))
+    return dataset
+
+
+def read_rainfalls(weather_filename):
+    dataset = []
+    with open (weather_filename) as f:
+        lines = f.readlines()
+        for line in lines[1:]:
+            tokens = line.split(",")
+            dataset.append(float(tokens[9]))
+        return dataset
+
+def days(rainfalls):
+    count = 0
+    for rainfall in rainfalls:
+        if rainfall >= 5:
+            count += 1
+    return count
+
+
+
+def main():
+    weather_filename = "lec/lec09/weather(146)_2022-2022.csv"
+    tavgs  = read_tavgs(weather_filename)
+    print(f"연 평균 온도는 {sum(tavgs)/len(tavgs):.1f}")
+
+    rainfalls  = read_rainfalls(weather_filename)
+    print(f"총 강수량은 {sum(rainfalls)}입니다.")
+    print(f"5mm이상 강우일수는 {days(rainfalls)}입니다.")
+if __name__ == "__main__":
+    main()
